f1105c30bee41a738c6b4aade9f2526851e7beab
Skyler Hochmuth <skyler.hochmuth@live.com>
2023-10-19 08:25:52 -0600

    Formatted Player Sign Card and Button Functionalit
    formatted the header card where player chooses sign some more. Added functionality to button clicks of choose player sign buttons. Now passes in player's sign to sign variable. Next will need to pass this into the Player function to create the player's sign to begin playing

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 //this is a module pattern - wrapping a factory function inside an IIFE (immediately invoked function expression)
 const gameBoard = (() => {
     const board = ['X', 'X', 'X', 'O', 'O', 'O', 'X', 'X', 'X'];
-    const setFields = (num, text) => {
+    const getFields = (num, text) => {
         // num is the index of the gameboard cells from left to right, top to bottom
         // text is the value that is placed in the field
         const htmlField = document.querySelector(`.game-board button:nth-child(${num+1}) p`);
         htmlField.innerHTML = text;
     }
-
-    return { board, setFields };
+    
+    return { board, getFields };
 })();
 
 const displayController = (() => {
+    const setFields = (() => {
+        //setFields uses getFields to loop through each applicable DOM element on the gameboard and populate with board array
+        for (let i=0; i<gameBoard.board.length; i++) {
+            gameBoard.getFields(i, gameBoard.board[i]);
+        }
+    })();
 
-})
 
+})();
 
-// event listener to populate the gameboard with board array
-for (let i=0; i<gameBoard.board.length; i++) {
-    gameBoard.setFields(i, gameBoard.board[i])
+const Player = (name, sign) => {
+    const playerName = name;
+    const playerSign = sign;
 
+    return { }
 }
 
+const gameController = (() => {
+    const signListener = (() => {
+        let btns = document.querySelectorAll('.sign-chooser')
+        console.log(btns)
+        btns.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                sign = e.target.innerHTML.toLowerCase();
+                console.log(sign)
+            })
+        })
+    })()
+})();
 
-document.getElementById('gameboard').innerHTML = gameBoard.board;
+
